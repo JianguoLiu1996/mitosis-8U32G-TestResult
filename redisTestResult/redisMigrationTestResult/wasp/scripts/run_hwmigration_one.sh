99e17239c864e57a8f6cc68f401c509b194d46fc
JianguoLiu1996 <491925717@qq.com>
2023-08-07 14:31:51 +0800

    change script to redis migration test, 20230807.

--- redisTestResult/redisMigrationTestResult/wasp/scripts/run_hwmigration_one.sh
+++ redisTestResult/redisMigrationTestResult/wasp/scripts/run_hwmigration_one.sh
@@ -95,14 +95,14 @@ prepare_basic_config_params()
 	# fi
 
         if [ $1 == "LPLD" ]; then
-                #CPU_NODE=3
-                #DATA_NODE=3
-                #PT_NODE=-6
+                CPU_NODE=3
+                DATA_NODE=3
+                PT_NODE=-6
 
 		# NoCross-nodeLossExperiment
-                CPU_NODE=0
-                DATA_NODE=0
-                PT_NODE=-3
+                #CPU_NODE=0
+                #DATA_NODE=0
+                #PT_NODE=-3
 		echo "qu : config is LPLD"
         fi
 
@@ -326,8 +326,8 @@ launch_benchmark_config()
 #     NODE_MAX=$(echo ${NODESTR##*: } | cut -d " " -f 1)
 #     NODE_MAX=`expr $NODE_MAX - 1`
     if [[ $LAST_CHAR == "M" ]]; then
-        #CMD_PREFIX+=" --pgtablerepl=3"
-        CMD_PREFIX+=" --pgtablerepl=0" # NoCross-nodeLossExperiment
+        CMD_PREFIX+=" --pgtablerepl=3"
+        #CMD_PREFIX+=" --pgtablerepl=0" # NoCross-nodeLossExperiment
     fi
     echo "CMD_PREFIX=$CMD_PREFIX"
     
